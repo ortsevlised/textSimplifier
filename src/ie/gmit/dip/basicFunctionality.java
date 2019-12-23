dc4b74e6b5e024350d502c517c74c75071d38857
Jorge <jdesilvestro@gmail.com>
2019-12-23 19:47:41 +0000

    have to leave the office so just pushing some small refactor so i can continue from the laptop

--- src/ie/gmit/dip/basicFunctionality.java
+++ src/ie/gmit/dip/basicFunctionality.java
@@ -1,40 +1,45 @@
 package ie.gmit.dip;
 
 import java.io.*;
-import java.util.Map;
-import java.util.Set;
-import java.util.TreeMap;
-import java.util.TreeSet;
+import java.util.*;
 
 public class basicFunctionality {
 
-    private Map<String, String> map = new TreeMap<>();
-    private Set<String> set = new TreeSet<>();
-
+    private static Map<String, String> map = new HashMap<>();
+    private static Set<String> set = new HashSet<>();
 
 
     //mapp of the google 100 words to a thersarus map
     //if the word does exist ignore/
     //add key listener to swap words
+    // add
+    private static final String googleWordFile = "C:\\Users\\Jorge\\Desktop\\gmit\\Text Simplifier API\\google-1000\\google-1000.txt";//ui ask the user for the file
+    private static final String thesaurusFile = "C:\\Users\\Jorge\\Desktop\\gmit\\Text Simplifier API\\MobyThesaurus2\\MobyThesaurus2.txt";
 
-    private static final String googleWordFile = null;//ui ask the user for the file
-    private static final String thesaurusFile = null;
+    //get line
+    // split by comma
+    // check in the google 100 list and map or ignore it
 
-    public void getLinesFromThesauros() throws IOException {
-        //get line
-        // split by comma
-        // check in the google 100 list and map or ignore it
+
+    public static void main(String[] args) throws Exception {
+        addGoogleWordsToMap();
+        addMobyThesaurus();
+        System.out.println("akak");
+    }
+    public static void addGoogleWordsToMap() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(new File(googleWordFile))));
-
-        String line = null;
-
-        while ((line = br.readLine()) != null) {
+        //String line;
+       // while ((line = br.readLine()) != null) {
+        for (String line = br.readLine(); line != null; line = br.readLine()) {
             map.put(line, line);
+            set.add(line);
         }
+    }
 
+    public static void addMobyThesaurus() throws IOException {
         BufferedReader brThesaurus = new BufferedReader(new InputStreamReader(new FileInputStream(new File(thesaurusFile))));
-
-        while ((line = br.readLine()) != null) {
+        String line;
+        while ((line = brThesaurus.readLine()) != null) {
             String[] words = line.split(",");
             String googleWord = null;
 
@@ -47,17 +52,17 @@ public class basicFunctionality {
             if (googleWord != null) {
                 addAll(words, googleWord);
             }
-            map.put(line, line);
         }
     }
 
-    private void addAll(String[] words, String googleWord) {
+
+    private static void addAll(String[] words, String googleWord) {
         for (String word : words) {
             map.put(word, googleWord);
         }
     }
 
-    public String getGoogleEquivalent(String word) {
+    public static String getGoogleEquivalent(String word) {
         if (map.containsKey(word)) {
             return map.get(word);
         } else {
